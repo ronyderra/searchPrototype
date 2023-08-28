8076edf3fd614ef12c7cf42b83e5b183ce5d9424
ronyderra <derrarony@gmail.com>
2023-08-28 15:27:06 +0300

    getCmp

--- src/controller/getCmp.ts
+++ src/controller/getCmp.ts
@@ -1,11 +1,16 @@
 import CMP from "../models/cmpJson";
 import { ICMPDocument } from "../models/interfaces/cmpJson";
+import { decrypt } from "../utils/crypto";
 
 export const getCmp = async (req: any, res: any) => {
   try {
     const { id } = req.query;
-    let result: ICMPDocument;
-
+    const dataStr = decrypt(id);
+    const cmpId = dataStr.split("-")[0];
+    const arId = dataStr.split("-")[1];
+    const result: ICMPDocument = await CMP.getByCmpIdAndArId(cmpId, arId);
+    console.log(result);
+    res.status(200).json({ result });
     return;
   } catch (error: any) {
     res.status(500).json({ message: error.toString() });
