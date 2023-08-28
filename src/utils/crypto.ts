d136fc403bd537e8b2424539cc50eddf779db344
ronyderra <derrarony@gmail.com>
2023-08-28 13:43:45 +0300

    encrypt decrypt

--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -2,21 +2,22 @@ import crypto from "crypto";
 
 const algorithm = "aes-256-cbc";
 const initVector = crypto.randomBytes(16);
-const message = "1-1693218184";
 const Securitykey = crypto.randomBytes(32);
 
-export function combineUUIDs(uuid1: string, uuid2: string) {
+export function encrypt(cmpId: string, ts: string) {
+  const message = `${cmpId}-${ts}`;
   const cipher = crypto.createCipheriv(algorithm, Securitykey, initVector);
   let encryptedData = cipher.update(message, "utf-8", "hex");
   encryptedData += cipher.final("hex");
   console.log("Encrypted message: " + encryptedData);
-  splitCombinedUUIDs(encryptedData);
+  return encryptedData;
 }
 
 // Function to decrypt and split the combined UUIDs
-export function splitCombinedUUIDs(encryptedData: any) {
+export function decrypt(encryptedData: string) {
   const decipher = crypto.createDecipheriv(algorithm, Securitykey, initVector);
   let decryptedData = decipher.update(encryptedData, "hex", "utf-8");
   decryptedData += decipher.final("utf8");
   console.log("Decrypted message: " + decryptedData);
+  return decryptedData;
 }
